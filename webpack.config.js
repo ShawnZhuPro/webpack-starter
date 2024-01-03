326738e90a25caf51da89cc333b12ab6c9b0fe26
ShawnZhuPro <114783966+ShawnZhuPro@users.noreply.github.com>
2024-01-02 17:25:26 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+// CommonJS syntax //
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["./src/index.js"],
+  output: {
+    // The path we are using for output is the "dist" folder
+    path: path.resolve(__dirname, "dist"),
+    // By default, the filename is "main.js" but we can change it
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    // Loaders go here
+    rules: [
+      {
+        // Looks for any file that ends with ".css" and applies the style + css loaders
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
